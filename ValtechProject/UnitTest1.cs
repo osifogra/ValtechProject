66f5fb62788ed3f0f6a7f9c8c4233f46316dd67d
graham osifo <Graham.osifo@gmail.com>
2018-12-02 09:28:43 +0000

    Valtech project

---
+++ ValtechProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ValtechProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.valtech.co.uk/");
+
+        }
+    }
+}
